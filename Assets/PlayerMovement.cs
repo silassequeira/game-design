95bdd79aaccaf809e9f68a4ff0b7fabf1d8b2df4
Beatriz Silva <beatrizsilva2002@gmail.com>
2025-04-14 16:12:04 +0100

    Implemented walking animations when player moves

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -3,18 +3,32 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     Rigidbody2D rb;
+    Animator anim;
+    SpriteRenderer spr;
     float moveForce=2;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         rb=GetComponent<Rigidbody2D>();
-        
+        anim=GetComponent<Animator>();
+        spr=GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
         rb.AddForce(Vector2.right*moveForce*Input.GetAxis("Horizontal"), ForceMode2D.Force);
-        
+        if(rb.linearVelocity.magnitude>0.01f){
+            anim.SetBool("isWalking",true);
+        }
+        else{
+            anim.SetBool("isWalking",false);
+        }
+        if(rb.linearVelocity.x<0){
+            spr.flipX=true;
+        }
+        else{
+            spr.flipX=false;
+        }
     }
 }
